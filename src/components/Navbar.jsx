acc74197822e698bdaaffb4b300ce328329d74c8
bobTheBuilderCodes <91603698+bobTheBuilderCodes@users.noreply.github.com>
2022-09-05 14:16:20 +0000

    Initial commit
    This is the beginning of the project

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="bg-slate-100 h-16 flex items-center p-4">
+      <h3 className="font-bold text-left text-slate-500 text-2xl flex justify-">
+        Tracker
+      </h3>
+    </div>
+  );
+};
+
+export default Navbar;
